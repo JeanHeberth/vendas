ebe1e057c148dd3f738ffd62bba0b078781b1089
Jean Heberth <jeanheberth19@gmail.com>
2022-07-05 21:15:51 -0300

    Criado o entity do produto.

---
+++ src/main/java/com/github/jeanheberth/vendas/modelo/entity/Produto.java
@@ -0,0 +1,37 @@
+package com.github.jeanheberth.vendas.modelo.entity;
+
+import java.math.BigDecimal;
+
+public class Produto {
+
+    private Integer id;
+    private String descricao;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public BigDecimal getPreco() {
+        return preco;
+    }
+
+    public void setPreco(BigDecimal preco) {
+        this.preco = preco;
+    }
+
+    private BigDecimal preco;
+
+
+}
